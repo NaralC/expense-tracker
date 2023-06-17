8cfcb7c0600f20923b430b68a14049d376b40e68
Naral <77269201+NaralC@users.noreply.github.com>
2023-06-17 23:45:30 +0700

    UI skeleton for dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,25 @@ import DashboardExample from "@/components/dashboard";
 import { MenubarDemo } from "@/components/menubar-demo";
 import Image from "next/image";
 
-async function getPosts() {
-  const res = await fetch(`${process.env.BASE_URL}/api`);
+// async function getPosts() {
+//   const res = await fetch(`${process.env.BASE_URL}/api`);
 
-  if (!res.ok) {
-    console.log(res);
-    throw new Error("Failed to fetch");
-  }
+//   if (!res.ok) {
+//     console.log(res);
+//     throw new Error("Failed to fetch");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
 export default async function Home() {
-  const data = await getPosts();
+  // const data = await getPosts();
 
   return (
     <>
-      {JSON.stringify(data)}
-      {/* <DashboardExample /> */}
-      {/* <MenubarDemo /> */}
+      {/* {JSON.stringify(data)} */}
+      <DashboardExample />
+      <MenubarDemo />
     </>
   );
 }
